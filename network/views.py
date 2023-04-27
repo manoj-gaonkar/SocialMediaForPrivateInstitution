2e85fbad068bbaccb9b59ac83e644575d117d47e
manoj <manojgaonkar7648@gmail.com>
2023-04-27 20:00:26 +0530

    edit post and delete post working properly

--- network/views.py
+++ network/views.py
@@ -224,6 +224,7 @@ def profile(request, username):
 
         
     return render(request, 'network/profile.html', {
+        "all_posts": all_posts,
         "username": user,
         "posts": posts,
         "posts_count": all_posts.count(),
@@ -269,6 +270,7 @@ def explore(request):
             user_mode = True
             
         return render(request, "network/explore.html", {
+            "all_posts": all_posts,
             "posts": posts,
             "suggestions": suggestions,
             "page": "explore",
@@ -310,6 +312,7 @@ def saved(request):
             user_mode = True
 
         return render(request, "network/index.html", {
+            "all_posts": all_posts,
             "posts": posts,
             "suggestions": suggestions,
             "page": "saved",
@@ -344,40 +347,27 @@ def create_post(request):
 @csrf_exempt
 def edit_post(request, post_id):
     if request.method == 'POST':
-        text = request.POST.get('text')
-        pic = request.FILES.get('picture')
-        img_chg = request.POST.get('img_change')
-        post_id = request.POST.get('id')
+        text = request.POST.get('newtext')
+        print(text)
+        # pic = request.FILES.get('picture')
+        # post_id = request.POST.get('id')
         post = Post.objects.get(id=post_id)
         try:
             post.content_text = text
-            if img_chg != 'false':
-                post.content_image = pic
+            if request.FILES.get('picture'):
+                post.content_image = request.FILES.get('picture')
             post.save()
             
-            if(post.content_text):
-                post_text = post.content_text
-            else:
-                post_text = False
-            if(post.content_image):
-                post_image = post.img_url()
-            else:
-                post_image = False
-            
-            return JsonResponse({
-                "success": True,
-                "text": post_text,
-                "picture": post_image
-            })
+            return HttpResponseRedirect(reverse('index'))
         except Exception as e:
             print('-----------------------------------------------')
-            print(e)
+            print(e,"edit post error")
             print('-----------------------------------------------')
             return JsonResponse({
                 "success": False
             })
     else:
-            return HttpResponse("Method must be 'POST'")
+            return HttpResponse("Method must be 'POST' rerr")
 
 @csrf_exempt
 def like_post(request, id):
